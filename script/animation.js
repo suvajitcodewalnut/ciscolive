eb39a5287b2912a558fcdf8ac8c43c2975a8772f
lovepreet.singh <lovepreet.singh@consensus.com>
2025-03-11 12:32:53 +0530

    fix: animation run after section came into viewport

--- script/animation.js
+++ script/animation.js
@@ -1,34 +1,11 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[scroll-container]"),
-  smooth: true,
-});
-
-// ANIMATE ON SCROLL
-// document.addEventListener("DOMContentLoaded", () => {
-//   const elements = document.querySelectorAll(".animate-element");
-
-//   const observerOptions = {
-//     root: null,
-//     rootMargin: "0px 0px -20% 0px",
-//     threshold: 0.3,
-//   };
-
-//   const observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.remove("animate-element");
-//         observer.unobserve(entry.target);
-//       }
-//     });
-//   }, observerOptions);
-
-//   elements.forEach((element) => observer.observe(element));
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("[scroll-container]"),
+//   smooth: true,
 // });
 
-// ANIMATE ON SCROLL : ANIMATION NONE PROPERTY
+// ANIMATE ON SCROLL
 document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll(".animation-onscroll-paused");
-  console.log(elements);
 
   const observerOptions = {
     root: null,
@@ -44,5 +21,28 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }, observerOptions);
+
   elements.forEach((element) => observer.observe(element));
 });
+
+// ANIMATE ON SCROLL : ANIMATION NONE PROPERTY
+// document.addEventListener("DOMContentLoaded", () => {
+//   const elements = document.querySelectorAll(".animation-onscroll-paused");
+//   console.log(elements);
+
+//   const observerOptions = {
+//     root: null,
+//     rootMargin: "0px 0px -20% 0px",
+//     threshold: 0.3,
+//   };
+
+//   const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.remove("animation-onscroll-paused");
+//         observer.unobserve(entry.target);
+//       }
+//     });
+//   }, observerOptions);
+//   elements.forEach((element) => observer.observe(element));
+// });
