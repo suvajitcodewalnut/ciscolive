7b717ee2fa99f5ce19c8e6daee0d7f75b3c2e9e3
suvajitcodewalnut <suvajit@codewalnut.com>
2025-03-14 17:35:05 +0530

    fix(carousel-transition-animation): carousel card change animation fixed

--- script/carousel.js
+++ script/carousel.js
@@ -1,25 +1,19 @@
-// CAROUSEL LOGIC 
+// CAROUSEL LOGIC
 const emblaNode = document.querySelector(".speakers__right");
-const options = { loop: true, align: "start" };
+const options = { loop: false, align: "start" };
 const emblaApi = EmblaCarousel(emblaNode, options);
 
 const nextButton = document.querySelector(".speakers__toggle--arrowright");
 const prevButton = document.querySelector(".speakers__toggle--arrowleft");
 
-nextButton.addEventListener("click", () => {
-  emblaApi.scrollNext();
-});
-
-prevButton.addEventListener("click", () => {
-  emblaApi.scrollPrev();
-});
-
 const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
   let dotNodes = [];
 
   const addDotBtnsWithClickHandlers = () => {
-    dotsNode.innerHTML = emblaApi
-      .scrollSnapList()
+    dotsNode.innerHTML = Array.from({
+      length: emblaApi.slideNodes().length - 2,
+    })
+      .fill(9)
       .map(() => '<div role="button" class="dot" type="button"></div>')
       .join("");
 
@@ -56,3 +50,107 @@ addDotBtnsAndClickHandlers(
   emblaApi,
   document.querySelector(".speakers__toggle--dots")
 );
+
+nextButton.addEventListener("click", () => {
+  const slidesInView = emblaApi.slidesInView();
+  const allSlideNodes = emblaApi.slideNodes();
+
+  const firstSlideIndex = slidesInView.at(0);
+  const lastSlideIndex = slidesInView.at(-1);
+
+  const currentFirstSlide = allSlideNodes[firstSlideIndex];
+  const nextSlides = allSlideNodes.slice(
+    firstSlideIndex + 1,
+    lastSlideIndex + 2
+  );
+
+  if (lastSlideIndex === allSlideNodes.length - 1) {
+    // emblaApi.scrollTo(0);
+    return;
+  }
+
+  const allSlides = [currentFirstSlide, ...nextSlides];
+
+  currentFirstSlide.setAttribute("data-animating", "true");
+  currentFirstSlide.style.opacity = "0";
+  currentFirstSlide.style.transform = "translateX(calc(-100% - 28px))";
+
+  currentFirstSlide.addEventListener(
+    "transitionend",
+    () => {
+      nextSlides.forEach((slide, index) => {
+        slide.setAttribute("data-animating", "true");
+        slide.style.transform = "translateX(calc(-100% - 28px))";
+        slide.style.transitionDelay = `${index * 0.2}s`;
+      });
+    },
+    { once: true }
+  );
+
+  const lastVisibleElem = nextSlides.at(-1);
+
+  lastVisibleElem.addEventListener(
+    "transitionend",
+    () => {
+      allSlides.forEach((slide) => {
+        slide.setAttribute("data-animating", "false");
+        slide.style = "null";
+      });
+      emblaApi.scrollNext(true);
+    },
+    { once: true }
+  );
+});
+
+prevButton.addEventListener("click", () => {
+  const slidesInView = emblaApi.slidesInView();
+  const allSlideNodes = emblaApi.slideNodes();
+
+  const firstSlideIndex = slidesInView.at(0);
+  const lastSlideIndex = slidesInView.at(-1);
+
+  const currentLastSlide = allSlideNodes[lastSlideIndex];
+  const prevSlides = allSlideNodes
+    .slice(firstSlideIndex - 1, lastSlideIndex)
+    .reverse();
+
+  if (firstSlideIndex === 0) {
+    // emblaApi.scrollTo(emblaApi.slideNodes().length - 1);
+    return;
+  }
+
+  const allSlides = [currentLastSlide, ...prevSlides];
+
+  currentLastSlide.setAttribute("data-animating", "true");
+  currentLastSlide.style.opacity = "0";
+  currentLastSlide.style.transform = "translateX(calc(100% + 28px))";
+
+  currentLastSlide.addEventListener(
+    "transitionend",
+    () => {
+      prevSlides.forEach((slide, index) => {
+        slide.setAttribute("data-animating", "true");
+        slide.style.transform = "translateX(calc(100% + 28px))";
+        slide.style.transitionDelay = `${index * 0.2}s`;
+      });
+    },
+    { once: true }
+  );
+
+  const firstVisibleElem = prevSlides.at(-1);
+  console.log(firstVisibleElem);
+
+  firstVisibleElem.addEventListener(
+    "transitionend",
+    () => {
+      allSlides.forEach((slide) => {
+        slide.setAttribute("data-animating", "false");
+        slide.style = "null";
+        // slide.style.opacity = "1";
+        // slide.style.transform = "translateX(0)";
+      });
+      emblaApi.scrollPrev(true);
+    },
+    { once: true }
+  );
+});
